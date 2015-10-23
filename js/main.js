6c9924fad3e84d3f773735fa6bdb5083e14b0b05
karenannelove <karenannelove@gmail.com>
2015-10-23 14:01:17 -0400

    added styling to the top navigation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//var linky = function() {
+
+//	$('.menu li a').css('color', #FFD94E)
+//};
+
+//$('.menu li a').on('.hover', linky);
+
+ //$('.menu li a').hover(function(){
+
+      //  $('.menu li a', this).css(color:#FFD94E);
+
+   // });
